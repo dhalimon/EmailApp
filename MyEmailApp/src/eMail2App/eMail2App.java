61724c628146b428dd24a055c611055cd32fdbed
dhalimon <Rome@206>
2020-06-19 09:31:18 -0400

    Initial commit -works

---
+++ MyEmailApp/src/eMail2App/eMail2App.java
@@ -0,0 +1,15 @@
+package eMail2App;
+
+public class eMail2App {
+
+        public static void main(String[] args){
+        eMail2 em1 = new eMail2 ("John", "Smith");
+
+//        em1.setAlternateEmail("js@live.com");
+//            System.out.println(em1.getAlternateEmail());
+            System.out.println(em1.showInfo());
+
+    }
+
+}
+
